511cb3ed9d66a2304a3145f3b99e4828f48d11f4
Carlos <carlosrodriguezbotello.guadalupe@alumnado.fundacionloyola.net>
2025-02-28 10:18:57 +0100

    eliminar spec.ts

--- src/app/directives/coche.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CocheDirective } from './coche.directive';
-
-describe('CocheDirective', () => {
-  it('should create an instance', () => {
-    const directive = new CocheDirective();
-    expect(directive).toBeTruthy();
-  });
-});
